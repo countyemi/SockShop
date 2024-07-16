ac0de83bb701995b6bbb222b422ebf376c61e48c
countyemi <adeyemiadeleye@rocketmail.com>
2024-07-15 21:50:23 -0400

    Update backend.tf

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -5,7 +5,7 @@ terraform {
     key            = "eks-cluster/terraform.tfstate"  
     region         = "us-east-1"
     encrypt        = true
-    dynamodb_table = "terraform-state-lock" 
+    
   }
 
 }
