fc3a443074efa875b1e6d73d5b180b63a69873d1
countyemi <adeyemiadeleye@rocketmail.com>
2024-07-18 19:05:57 -0400

    workflow updated

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,7 +1,7 @@
 terraform {
 
   backend "s3" {
-    bucket         = "bald-eagle-sockshop-state-2433"
+    bucket         = var.bucket_name
     key            = "eks-cluster/terraform.tfstate"  
     region         = "us-east-1"
     encrypt        = true
